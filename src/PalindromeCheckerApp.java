c29001fafdb2706ae77f14fddcd311415e805dff
Ayush <fayasfaya6677@gmail.com>
2026-02-24 16:02:58 +0530

    UC3:String Reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,32 +1,23 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
+        // Hardcoded string
+        String original = "madam";
 
+        // Variable to store reversed string
+        String reversed = "";
 
-                // Hardcoded String (String Literal)
-                String word = "madam";
-
-                // Variable to store result
-                boolean isPalindrome = true;
-
-                int length = word.length();
-
-                // Checking palindrome condition
-                for (int i = 0; i < length / 2; i++) {
-                    if (word.charAt(i) != word.charAt(length - i - 1)) {
-                        isPalindrome = false;
-                        break;
-                    }
-                }
-
-                // Printing result using if-else
-                if (isPalindrome) {
-                    System.out.println("The given string \"" + word + "\" is a Palindrome.");
-                } else {
-                    System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
-                }
-
-            }
+        // Reverse using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
+        // Compare original and reversed string
+        if (original.equals(reversed)) {
+            System.out.println("The given string \"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + original + "\" is NOT a Palindrome.");
+        }
 
+    }
+}
