525bd0c68cabe941d05646fdddd8c2550b2d9692
Ayush <fayasfaya6677@gmail.com>
2026-02-24 15:28:54 +0530

    uc1 - welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to the Palindrome Checker management system");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
     }
 }
