e109bc8340ee82649136e059b0bd6d3bf9697a97
yamini757 <yaminiguleria072@gmail.com>
2026-03-11 15:19:00 +0530

    UC12: Strategy Pattern for Palindrome Algorithms (Advanced)

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,34 +1,76 @@
+import java.util.*;
+
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean checkPalindrome(String word);
+}
+
+// Stack Strategy
+class StackStrategy implements PalindromeStrategy {
+    public boolean checkPalindrome(String word) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Deque Strategy
+class DequeStrategy implements PalindromeStrategy {
+    public boolean checkPalindrome(String word) {
+        Deque<Character> deque = new LinkedList<>();
+
+        for (char c : word.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// Context Class
+class PalindromeChecker {
+    private PalindromeStrategy strategy;
+
+    public PalindromeChecker(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean check(String word) {
+        return strategy.checkPalindrome(word);
+    }
+}
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        // Hardcoded string
         String word = "madam";
 
-        // Call recursive function
-        boolean isPalindrome = checkPalindrome(word, 0, word.length() - 1);
+        // Choose strategy dynamically
+        PalindromeStrategy strategy = new StackStrategy();
+        // PalindromeStrategy strategy = new DequeStrategy();
 
-        // Print result
-        if (isPalindrome) {
+        PalindromeChecker checker = new PalindromeChecker(strategy);
+
+        boolean result = checker.check(word);
+
+        if (result) {
             System.out.println("The given string \"" + word + "\" is a Palindrome.");
         } else {
             System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
         }
-    }
-
-    // Recursive method
-    public static boolean checkPalindrome(String str, int start, int end) {
-
-        // Base condition
-        if (start >= end) {
-            return true;
-        }
-
-        // If characters do not match
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return checkPalindrome(str, start + 1, end - 1);
     }
 }
